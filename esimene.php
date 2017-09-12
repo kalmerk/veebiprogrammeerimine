47e52aea6321761a2f0561c96f1d919a9b7b9a96
Kalmer Kaarjas <kalmerk@users.noreply.github.com>
2017-09-12 14:15:42 +0300

    Lisatud on esimene jupp PHP koodi

---
+++ esimene.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <title>Kalmer Kaarjas veebiprogrammeerimine</title>
+</head>
+<body>
+    <h1>Kalmer Kaarjas veebiprogrammeerimine</h1>
+            <p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiseltvõetavat sisu.</p>
+    <h2>Informatsioon</h2>
+        <p>Esimene info.</p>
+        <p>Teine info.</p>
+        <p>Kolmas info.</p>
+	<?php
+		echo "See on esimene jupp PHP abil väljastatud tektsti!";
+	?>
+</body>
+</html>
